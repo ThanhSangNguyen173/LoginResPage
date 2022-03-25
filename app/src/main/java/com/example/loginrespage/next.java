71eb2a38cf2089d3e98116573658b0d52ac5a8b2
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-03-25 14:17:23 +0700

    25/3 Commit

---
+++ app/src/main/java/com/example/loginrespage/next.java
@@ -0,0 +1,14 @@
+package com.example.loginrespage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class next extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
